418ce07d3ed7e2fd7b2af29d1dbbbdac1b4fe5b9
RareTC <trevorjcampbell@aol.com>
2023-01-21 16:58:25 -0800

    constants, state var, and init function

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-  /*----- constants -----*/
+/*----- constants -----*/
+const CARDS = {}
+const WINNING MATCHES = {}
+
+/*----- state variables -----*/
+let points;
+let timer;
+let board;
+
+/*----- cached elements  -----*/
 
 
-  /*----- state variables -----*/
+/*----- event listeners -----*/
 
 
-  /*----- cached elements  -----*/
+/*----- functions -----*/
+init ();
 
-
-  /*----- event listeners -----*/
-
-
-  /*----- functions -----*/
+function init () {
+    board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    timer = //2:00min;
+    points = '0';
+    render ();
+}
